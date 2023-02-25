f7bb7f2774d098133aefcaf192b52fd65a172a66
lkusmir <lkusmir@gmail.com>
2023-02-25 19:39:41 +0100

    special attributes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,6 +5,8 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
     with tracer.start_as_current_span("home-activities-mock-data"):
+      span = trace.get_current_span()
+      span.set_attribute("app.now", now.isoformat())
       now = datetime.now(timezone.utc).astimezone()
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
@@ -45,4 +47,5 @@ class HomeActivities:
         'replies': []
       }
       ]
+      span.set_attribute("app.result_lenth", len(results))
       return results
