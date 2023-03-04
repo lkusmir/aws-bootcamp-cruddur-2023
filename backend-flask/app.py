9c4fbbe830f04510933cb0851d82585fcb09132c
lkusmir <lkusmir@gmail.com>
2023-03-04 23:01:33 +0100

    rollback config added

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,6 +33,12 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 # from time import strftime
 #EOF Cloudwatch
 
+# Rollbar
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # HoneyComb: Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
@@ -62,6 +68,24 @@ RequestsInstrumentor().instrument()
 # xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 # XRayMiddleware(app, xray_recorder)
 
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#EOF ROllbar
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -84,6 +108,13 @@ cors = CORS(
 @app.route("/api/status", methods=['GET'])
 def data_show_testsuccess():
   return "OK", 200
+
+# Rollbar test
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+#Eof Rollbar test
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
