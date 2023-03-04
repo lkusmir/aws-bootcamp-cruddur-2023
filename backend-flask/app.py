fd05215cace58544f6f5357bd82af1eb8928302e
lkusmir <lkusmir@gmail.com>
2023-03-04 10:39:24 +0100

    xray config

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,6 +21,11 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+# X-Ray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # HoneyComb: Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -34,6 +39,12 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 # Eof Honeycomb
+
+# X-ray starting of decoder
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
