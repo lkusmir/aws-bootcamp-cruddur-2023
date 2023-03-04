53d3872db5d61079fbe630a4fade7b279a21a75b
lkusmir <lkusmir@gmail.com>
2023-03-04 17:27:50 +0100

    cloudwatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -27,12 +27,26 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 # from aws_xray_sdk.core import xray_recorder
 # from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# CloudWatch
+import watchtower
+import logging
+from time import strftime
+
 # HoneyComb: Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# CloudWatch
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
 
 app = Flask(__name__)
 # HoneyComb
@@ -55,6 +69,14 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#Cloudwatch
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+#EOF Cloudwatch
 
 @app.route("/api/status", methods=['GET'])
 def data_show_testsuccess():
